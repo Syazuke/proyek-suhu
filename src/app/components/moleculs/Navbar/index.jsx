7cb57b3822277a16f3a73273b2ae481d2a2e411e
Syazuke <ssyazukee@gmail.com>
2026-04-30 18:56:02 +0700

    update

--- src/app/components/moleculs/Navbar/index.jsx
+++ src/app/components/moleculs/Navbar/index.jsx
@@ -1,64 +1,61 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 const Navigation = () => {
   const pathname = usePathname();
   if (pathname.endsWith("/")) {
     return null;
   }
-
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="bg-white text-black shadow-sm top-0 z-50">
-      <div className="mx-auto px-4 py-3">
+    <nav className="bg-white text-black shadow-sm sticky top-0 z-50">
+      <div className="mx-auto px-4 py-3 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-2">
-          <div className="flex justify-center items-center">
-            <h1 className="text-3xl">Suhu</h1>
-          </div>
-          <div className="hidden md:inline space-x-6">
+          <h1>Suhu</h1>
+          <div className="hidden md:inline space-x-10">
             <a href="/" className="py-4 hover:text-hover">
               Home
             </a>
             <a href="/simulator" className="py-4 hover:text-hover">
-              Simulasi
+              simulasi
             </a>
-            <a href="/rumus" className="py-4 mb-6 hover:text-hover">
-              Rumus
+            <a href="/info" className="py-4 hover:text-hover">
+              info
             </a>
-            <a href="/info" className="py-4 mb-6 hover:text-hover">
-              Info
+            <a href="/rumus" className="py-4 hover:text-hover">
+              rumus
             </a>
-            <a href="/about" className="py-4 mb-6 hover:text-hover">
-              Kami
+            <a href="/kami" className="py-4 hover:text-hover">
+              kami
             </a>
           </div>
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="text-2xl focus:outline-none md:hidden inline cursor-pointer"
+            className="text-2xl focus:outline-none md:hidden inline"
           >
             <i className={`fa-solid ${isOpen ? "fa-xmark" : "fa-bars"}`}></i>
           </button>
         </div>
         {isOpen && (
           <div
-            className={`absolute top-full left-0 w-full bg-white flex flex-col py-6 px-4 shadow-lg z-50 ${isOpen ? "border-b border-(--divider)" : ""}`}
+            className={`absolute top-full left-0 w-full text-black bg-white flex flex-col py-6 px-4 shadow-lg z-50 ${isOpen ? "border-b border-(--divider)" : ""}`}
           >
             <a href="/" className="py-4 hover:text-hover">
               Home
             </a>
             <a href="/simulator" className="py-4 hover:text-hover">
-              Simulasi
-            </a>
-            <a href="/rumus" className="py-4 hover:text-hover">
-              Rumus
+              simulasi
             </a>
             <a href="/info" className="py-4 hover:text-hover">
-              Info
+              info
             </a>
-            <a href="/about" className="py-4 hover:text-hover">
-              Kami
+            <a href="/rumus" className="py-4 hover:text-hover">
+              rumus
+            </a>
+            <a href="/kami" className="py-4 hover:text-hover">
+              kami
             </a>
           </div>
         )}
