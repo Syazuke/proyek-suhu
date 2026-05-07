3620705d8d2a5163c5b5cfe1f955cd95324d85e6
Gem002 <gemmyenjoyyourlife@gmail.com>
2026-05-07 15:58:10 +0700

    Gemmy_page kami

--- src/app/kami/page.jsx
+++ src/app/kami/page.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
-import pahlawan from "../../../public/pahlawan.png";
-import Yow from "../../../public/Yow.png";
+import Gemmy from "../../../public/Gemmy.png";
+import Fariz from "../../../public/Fariz.png";
+import Agnia from "../../../public/Agnia.png";
+import Rahman from "../../../public/Rahman.png";
+import Raivan from "../../../public/Raivan.png";
 
 
 const anggota = [
@@ -10,29 +13,29 @@ const anggota = [
     peran: "Frontend Developer",
     deskripsi:
       "Bertanggung jawab dalam pembuatan tampilan website menggunakan Next.js dan Tailwind CSS .",
-    foto: pahlawan
+    foto: Rahman
   },
   {
     nama: "Gemmy Dwirismariyan Hagi",
     nim: "257006111074",
     peran: "Backend Developer",
     deskripsi:
-      "Mengelola database dan API untuk memastikan data berjalan dengan baik.",
-    foto: Yow
+      "Merancang dan mendesain page Kami.",
+    foto: Gemmy
   },
   {
     nama: "Agnia Agustin Ramadhani",
     nim: "257006111090",
     peran: "UI/UX Designer",
     deskripsi: "Merancang desain antarmuka agar menarik dan mudah digunakan.",
-    foto: "/anggota3.jpg",
+    foto: Agnia
   },
   {
     nama: "Rafi Nurraivan",
     nim: "257006111103",
     peran: "UI/UX Designer",
     deskripsi: "Merancang desain antarmuka agar menarik dan mudah digunakan.",
-    foto: "/anggota3.jpg",
+    foto: Raivan
   },
   {
     nama: "Indah Kasih Lestari",
@@ -46,36 +49,36 @@ const anggota = [
     nim: "257006111117",
     peran: "UI/UX Designer",
     deskripsi: "Merancang desain antarmuka agar menarik dan mudah digunakan.",
-    foto: "/anggota3.jpg",
+    foto: Fariz
   },
 ];
 
 export default function KamiPage() {
   return (
-    <div className="min-h-screen bg-blue-100 py-10 px-5">
-      <h1 className="text-3xl font-bold text-center text-black mb-10">Tim Kami</h1>
+    <div className="min-h-screen bg-black py-10 px-5">
+      <h1 className="text-4xl font-bold text-center text-white mb-10">Tim Kami</h1>
 
       <div className="grid md:grid-cols-3 gap-8">
         {anggota.map((item, index) => (
           <div
             key={index}
-            className="bg-black rounded-2xl shadow-lg p-5 text-center hover:shadow-2xl transform hover:scale-105 transition duration-300"
+            className="bg-gray-900 rounded-2xl shadow-lg p-5 text-center hover:shadow-2xl transform hover:scale-105 transition duration-300"
           >
-            <div className="w-40 h-40 mx-auto mb-4 relative overflow-hidden rounded-2xl group">
+            <div className="w-50 h-50 mx-auto mb-4 relative overflow-hidden rounded-2xl group">
               <Image
                 src={item.foto}
                 alt={item.nama}
                 fill
-                className="object-cover object-center transition duration-300 group-hover:scale-110"
+                className="object-cover object-top transition duration-300 group-hover:scale-110"
               />
             </div>
 
-            <h2 className="text-xl font-semibold">{item.nama}</h2>
-            <p className="text-gray-500 font-semibold">{item.nim}</p>
+            <h2 className="text-xl bg-gray-800 rounded-2xl font-semi bold">{item.nama}</h2>
+            <p className="text-white-500 bg-gray-800 rounded-2xl font-semi bold mt-2">{item.nim}</p>
 
-            <p className="mt-2 font-semi bold text-green-600">{item.peran}</p>
+            <p className="mt-7 font-semi bold text-blue-800">{item.peran}</p>
 
-            <p className="text-sm text-red-600 mt-2">{item.deskripsi}</p>
+            <p className="text-sm text-gray-400 rounded-2xl bg-gray-800 mt-2">{item.deskripsi}</p>
           </div>
         ))}
       </div>
